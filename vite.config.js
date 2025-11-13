55f7f61a7537691ca5f1b7ed24dfe5bdc871b0bc
Nadavrav <nadavrav@post.bgu.ac.il>
2025-11-13 20:57:34 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// C:\Code\BirthdaySite\birthdaysite\vite.config.js
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// חוזרים לנתיב השורש הטהור
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  // *** וודא שהשורה הזו קיימת ונכונה ***
+  base: '/BirthdaySite/', 
 })
